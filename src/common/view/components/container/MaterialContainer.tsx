3eae4b3a6b1e8320931544137a5301a3b5c98b94
fernando <f.sanchezvilela@gmail.com>
2021-07-25 21:45:56 +0200

    fix(material container): change material ui type

--- src/common/view/components/container/MaterialContainer.tsx
+++ src/common/view/components/container/MaterialContainer.tsx
@@ -3,7 +3,7 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import Container from '@material-ui/core/Container';
 
 export interface IMaterialContainer {
-  children: React.ReactChildren;
+  children: React.ReactNode;
 }
 
 const MaterialContainer = ({ children }: IMaterialContainer) => (
